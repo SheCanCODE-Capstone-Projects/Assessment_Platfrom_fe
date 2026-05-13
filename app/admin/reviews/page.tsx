96fd069372db77cf758fec151994e0cf75b45273
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-05-13 20:36:13 +0200

    000

--- app/admin/reviews/page.tsx
+++ app/admin/reviews/page.tsx
@@ -251,7 +251,7 @@ export default function CodeReviewsPage() {
         </div>
       </div>
 
-      {/* LIST */}
+      
       <main className="flex-1 space-y-4 px-4 py-6 sm:px-6 lg:px-8">
         {paginated.map((item) => (
           <div
